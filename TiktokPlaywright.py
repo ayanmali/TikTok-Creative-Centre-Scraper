a21670a72dd9d589a298c70c583d568302cd2045
Ayan Ali <ayan_ali2004@hotmail.com>
2024-09-02 09:44:18 -0400

    Playwright script to login

--- TiktokPlaywright.py
+++ TiktokPlaywright.py
@@ -1,20 +1,30 @@
 from playwright.sync_api import Playwright, sync_playwright
+import sys
+sys.path.insert(1, 'c:/Users/ayan_/Desktop/Desktop/Coding/Cursor Workspace/Scrapers')
 from tiktok_credentials import email, password
+import time
 
 def run(playwright: Playwright) -> None:
     main_url = "https://ads.tiktok.com/business/creativecenter/pad/en"
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
+    context = browser.new_context(ignore_https_errors=True, user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.81")
     page = context.new_page()
-    page.goto(main_url)
-    page.get_by_test_id("cc_header_login").click()
 
-    # with page.expect_popup() as page1_info:
-    #     page.get_by_text("Log in with Google").click()
-    # page1 = page1_info.value
-    # page1.get_by_label("Email or phone").click()
-    # page1.get_by_label("Email or phone").fill(email)
-    # page1.get_by_role("button", name="Next").click()
+    # Navigating to the page
+    page.goto(main_url)
+    # Clicking the login button
+    page.get_by_test_id("cc_header_login").click()
+    time.sleep(5)
+    #
+
+    page.get_by_text("Log in with phone/email").click()
+    page.get_by_placeholder("Enter your email address").click()
+    page.get_by_placeholder("Enter your email address").fill(email)
+    page.get_by_placeholder("Enter your password").click()
+    page.get_by_placeholder("Enter your password").fill(password)
+    page.click("#TikTok_Ads_SSO_Login_Btn")
+    
+    time.sleep(5)
 
     # ---------------------
     context.close()
--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-
-r = requests.get("")
